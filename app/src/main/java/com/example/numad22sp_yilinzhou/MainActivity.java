7e54aa6a00883a3962a30a8af25b0c5433f80aca
Yilin Zhou <zhou.yil@northeastern.edu>
2022-02-08 13:58:10 -0500

    Update for Assignment 3: Clicky Clicky

--- app/src/main/java/com/example/numad22sp_yilinzhou/MainActivity.java
+++ app/src/main/java/com/example/numad22sp_yilinzhou/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.numad22sp_yilinzhou;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-//    向下兼容的Activity 其中AppCompatActivity是Activity的子类 Activity是Android系统提供的一个活动基类
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         Button button_Panel = (Button)findViewById(R.id.buttonPanel);
+
 
         button_Panel.setOnClickListener(new View.OnClickListener() {
 
@@ -27,5 +28,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button Clicky = (Button)findViewById(R.id.Clicky_Clicky);
+
+        Clicky.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                openActivityClicky();
+            }
+        });
+
+    }
+    public void openActivityClicky(){
+        Intent intent = new Intent(this,Clicky.class);
+        startActivity(intent);
     }
 }
