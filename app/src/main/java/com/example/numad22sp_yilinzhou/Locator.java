ec21bd1bbd443d8a9a0e6421c0e69f89f5231ca9
Yilin Zhou <zhou.yil@northeastern.edu>
2022-02-20 23:14:13 -0500

    Update for Assignment 5: Locator

--- app/src/main/java/com/example/numad22sp_yilinzhou/Locator.java
+++ app/src/main/java/com/example/numad22sp_yilinzhou/Locator.java
@@ -1,5 +1,6 @@
 package com.example.numad22sp_yilinzhou;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import android.os.Bundle;
@@ -19,51 +20,69 @@ import androidx.core.content.ContextCompat;
 public class Locator extends AppCompatActivity {
     private LocationManager locationManager;
     private String locationProvider = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locator);
 
-        //location manager
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        //provider
-        List<String> providers = locationManager.getProviders(true);
-
-        //get permission
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED ||
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                         != PackageManager.PERMISSION_GRANTED) {
             //request
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
-        } else {
+                    Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
+        }else{
+            //location manager
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            //provider
+            List<String> providers = locationManager.getProviders(true);
             if (providers.contains(LocationManager.GPS_PROVIDER)) {
                 locationProvider = LocationManager.GPS_PROVIDER;
             } else if (providers.contains(LocationManager.NETWORK_PROVIDER)) {
                 locationProvider = LocationManager.NETWORK_PROVIDER;
-            }else {
+            } else {
                 Toast.makeText(this, "No Avaliable Locator", Toast.LENGTH_SHORT).show();
                 return;
             }
             //get location
-            locationManager.requestLocationUpdates(locationProvider, 100, 1,locationListener);
-            Location location = locationManager.getLastKnownLocation(locationProvider);
-            if (location!=null){
-                changeLocation(location);
+            locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
+        }
+    };
+
+    //callback after request permission
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //location manager
+                    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    //provider
+                    List<String> providers = locationManager.getProviders(true);
+                    if (providers.contains(LocationManager.GPS_PROVIDER)) {
+                        locationProvider = LocationManager.GPS_PROVIDER;
+                    } else if (providers.contains(LocationManager.NETWORK_PROVIDER)) {
+                        locationProvider = LocationManager.NETWORK_PROVIDER;
+                    } else {
+                        Toast.makeText(this, "No Avaliable Locator", Toast.LENGTH_SHORT).show();
+                    }
+                    //get location
+                    try {
+                        locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
+
+                    } catch (SecurityException e) {
+                    }
+                }
+                break;
             }
         }
-    }
-
-    public void changeLocation(Location location){
-        //new Longitude
-        TextView loshow=(TextView)findViewById(R.id.loshow);
-        loshow.setText(String.valueOf(location.getLongitude()));
-        //new Latitude
-        TextView lashow=(TextView)findViewById(R.id.lashow);
-        lashow.setText(String.valueOf(location.getLatitude()));
-    }
+    };
 
     public LocationListener locationListener = new LocationListener() {
         //status change
@@ -82,16 +101,13 @@ public class Locator extends AppCompatActivity {
         @Override
         public void onLocationChanged(Location location) {
             if (location != null) {
-                changeLocation(location);
+                TextView loshow=(TextView)findViewById(R.id.loshow);
+                loshow.setText(String.valueOf(location.getLongitude()));
+                //new Latitude
+                TextView lashow=(TextView)findViewById(R.id.lashow);
+                lashow.setText(String.valueOf(location.getLatitude()));
             }
         }
     };
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        locationManager.removeUpdates(locationListener);
-    }
-
 
 }
